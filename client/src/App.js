4c6466c38b8250eb0d0514ec3743ac1809587705
Adin Ashby <adin.ashby@gmail.com>
2023-09-18 15:20:59 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      {typeof backendData.users === "undefined" ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => <p key={i}>{user}</p>)
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("/api", (req, res) => {
+  res.json({ users: ["user1", "user2", "user3"] });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"), (err) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.listen(3002, () => {
+  console.log("Server is running on port 3002");
+});
